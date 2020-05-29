e030a42944eec46d8ea7019a1295bf6a0b029784
LanTenDragon <pantheonia1@gmail.com>
2020-05-29 11:11:36 +0800

    Add environment aware logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 require('dotenv').config()
+const Logger = require('./logger')
 const port = process.env.PORT || 8080
 const MongoURL = process.env.MONGO - URL || 'mongodb://localhost/test'
 
@@ -29,5 +30,5 @@ const server = app.listen(port, function () {
   const host = 'localhost'
   const port = server.address().port
 
-  console.log('App listening at http://%s:%s', host, port)
+  Logger('App listening at http://' + host + ':' + port)
 })
